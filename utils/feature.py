5b3daf1419a389763412be6de68c69673eb78c0d
mj-haghighi <nohammadjavad.developer@gmail.com>
2024-09-28 11:45:50 +0330

    Amend feature extractor to ignore classifier

--- utils/feature.py
+++ utils/feature.py
@@ -3,28 +3,30 @@ import torch
 import torch.nn.functional as F
 
 
-def extract_features(dataloader, encoder, classifier):
+def extract_features(dataloader, encoder, classifier=None):
     feature_bank = []
     preds_logits = []
     for (data, _, _) in tqdm(dataloader, desc='Feature extracting'):
         data = data.cuda()
         feature = encoder(data)
         feature_bank.append(feature)
-        batch_pred_logits = classifier(feature)
-        preds_logits.append(batch_pred_logits)
+        if classifier is not None:
+            batch_pred_logits = classifier(feature)
+            preds_logits.append(batch_pred_logits)
     feature_bank = F.normalize(torch.cat(feature_bank, dim=0), dim=1)
     return feature_bank, preds_logits
 
 
 
-def extract_cifar_features(dataloader, encoder, classifier):
+def extract_cifar_features(dataloader, encoder, classifier=None):
     feature_bank = []
     preds_logits = []
     for (data, _, _, _) in tqdm(dataloader, desc='Feature extracting'):
         data = data.cuda()
         feature = encoder(data)
         feature_bank.append(feature)
-        batch_pred_logits = classifier(feature)
-        preds_logits.append(batch_pred_logits)
+        if classifier is not None:
+            batch_pred_logits = classifier(feature)
+            preds_logits.append(batch_pred_logits)
     feature_bank = F.normalize(torch.cat(feature_bank, dim=0), dim=1)
     return feature_bank, preds_logits
